f92f38a50d2a32a509c99cad5c59a6ad567bb013
Artem Riabushenko <ariabushenko@blackbird.io>
2026-02-02 19:03:27 +0200

    Removed unused handlers

--- ClickUp/Webhooks/Handlers/Tasks/DataHandlers/TaskWebhookDataHandler.cs
+++ ClickUp/Webhooks/Handlers/Tasks/DataHandlers/TaskWebhookDataHandler.cs
@@ -1,11 +1,12 @@
 ﻿using Apps.ClickUp.DataSourceHandlers.Task;
+using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Invocation;
 
 namespace Apps.ClickUp.Webhooks.Handlers.Tasks.DataHandlers
 {
     public class TaskWebhookDataHandler : TaskDataHandler
     {
-        public TaskWebhookDataHandler(InvocationContext invocationContext, WebhookScopeRequest request) :
+        public TaskWebhookDataHandler(InvocationContext invocationContext, [ActionParameter] WebhookScopeRequest request) :
         base(invocationContext, request.ListId)
         {
 
